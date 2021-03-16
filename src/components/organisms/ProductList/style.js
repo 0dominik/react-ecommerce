91cd3ad977dea5ad1ed65a95e500a7e254a30138
Dominik <dmnsre@gmail.com>
2021-03-16 01:31:25 +0100

    remove redundant file

--- src/components/organisms/ProductList/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 30px 0;
-  max-width: 1500px;
-  justify-content: center;
-`;
